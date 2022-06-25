4c44f796dd4b98a342e7ea2712969fa4d4c4caf7
siyolisemekuto <103102826+siyolisemekuto@users.noreply.github.com>
2022-06-25 13:21:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Boolean to check local storage
 // localStorage.removeItem('records');
 let items = JSON.parse(localStorage.getItem('records'))
-? JSON.parse(localStorage.getItem('records')):[
+ ? JSON.parse(localStorage.getItem('records')):[
     {
         id: 1,
         item: 'TV Stand',
@@ -12,15 +12,16 @@ let items = JSON.parse(localStorage.getItem('records'))
      
 //function to show items
 function showItems(){
-    // itemContainer.innerHTML = "" 
 
+    document.querySelector('#item-wrapper').innerHTML = '';
 //looping (will keep reshowing items)
-    items.forEach(item => {
+    items.forEach((item,index) => {
         document.querySelector('#item-wrapper').innerHTML +=`
     <li class=item>
         <input type='checkbox' class='check'>
-        <span class='checkItem'>${item.item} </span>
-        <span>
+        <span class='checkItem'>${item.item}</span>  
+        <i id=edit type='button' onClick='editItem(${item})' class="fa-solid fa-pen"></i>
+        <i  id='delete' onclick='deleteRecord(${index})' class="fa-solid fa-circle-minus"></i>    
     </li> `
     });
     // console.table(items);
@@ -36,17 +37,24 @@ function crossedOut(item) {
 //delete function
 function deleteRecord(id) {
     if(id > -1){
-        lists.splice( id, 1);
-        // After delete
-        localStorage.setItem('records', JSON.stringify(lists) );
-    }else {
-        console.log('Name was not found');
+        items.splice(id, 1);
     }
+   localStorage.setItem('records', JSON.stringify(items));
+   showItems();
+}
+//edit function
+function editItem(item){
+    const editItem = {
+        id: items.length + 1,
+        item: document.querySelector('#task').value,
+        dateCreated: new Date()
+    };
+    console.log(editItem);
+    items.push(editItem);
+    localStorage.setItem('records', JSON.stringify(items)); 
 }
 
 function addItem(){
-    // console.log('Hello There');
-    // e.preventDefault();
     const newItem = {
         id: items.length + 1,
         item: document.querySelector('#task').value,
@@ -63,3 +71,13 @@ showItems();
 
 // Event listener
 document.querySelector('#addRecord').addEventListener('click', addItem);
+
+//sort-by functionality
+document.querySelector('#sorting').addEventListener('click', ()=> {
+    items.sort( (a, b)=> {
+        return (a.item < b.item) ? -1: 0;
+    });
+    // Save new data to the localstorage
+    localStorage.setItem('records', JSON.stringify(items));
+    readItems();
+});
